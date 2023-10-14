64d755edbb6e6f31c24aef32fd44e04f57ff6e72
Said Berk <csaidberk@gmail.com>
2023-10-14 13:35:33 +0300

    Kütüphane bağlantısı testi başarılı

---
+++ Test/sobatLib/sobat.cpp
@@ -0,0 +1,9 @@
+#include "Sobat.h"
+
+Sobat::Sobat() {
+  // Kurucu fonksiyon - İhtiyaç halinde burada başlangıç ayarlarını yapabilirsiniz.
+}
+
+void Sobat::sayHello() {
+  Serial.println("Merhaba, Sobat Kütüphanesi!");
+}
---
+++ Test/sobatLib/sobat.h
@@ -0,0 +1,12 @@
+#ifndef Sobat_h
+#define Sobat_h
+
+#include <Arduino.h>
+
+class Sobat {
+public:
+  Sobat();  // Kurucu fonksiyon
+  void sayHello();  // Örnek işlev
+};
+
+#endif
