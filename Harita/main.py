935fe5ec098c10648955468b114fbd328f80e38d
Said Berk <csaidberk@gmail.com>
2023-10-22 17:09:21 +0300

    Haritlaandırma Koduna QT eklendi ve Real-Time Functionality Sağlandı

--- Harita/main.py
+++ Harita/main.py
@@ -1,30 +1,53 @@
+import sys
+import random
+import time
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-harita_genislik = 20
-harita_yukseklik = 20
+class RealTimePlot(QMainWindow):
+    map_height = 20
+    map_width = 20
 
-fig, ax = plt.subplots()
-ax.set_aspect('equal')
+    def __init__(self):
+        super().__init__()
 
-ax.set_xlim(0, harita_genislik)
-ax.set_ylim(0, harita_yukseklik)
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.setCentralWidget(self.canvas)
 
-ax.grid(True, markevery=0.5)
+        self.ax = self.figure.add_subplot(111)
+        self.ax.set_xlim(0, self.map_height)
+        self.ax.set_ylim(0, self.map_width)
+        self.ax.set_xlabel('X (metre)')
+        self.ax.set_ylabel('Y (metre)')
 
-ax.set_xlabel('X (metre)')
-ax.set_ylabel('Y (metre)')
+        self.ax.grid(True, linestyle='-', linewidth=0.5, which='both')
 
-plt.title('Sobat Havuz Haritası')
+        self.timer = self.canvas.new_timer(100, [(self.update_plot, [], {})])
+        self.timer.start()
 
-x_grid = np.arange(0, harita_genislik, 1)  # Her 1 metrede bir x koordinatı
-y_grid = np.arange(0, harita_yukseklik, 1)  # Her 1 metrede bir y koordinatı
-ax.set_xticks(x_grid)
-ax.set_yticks(y_grid)
+        self.show()
 
+        # Harita üzerine resmi ekleyin
+        self.add_image_to_center("submarine.png")
 
-img = mpimg.imread('submarine.png')
-ax.imshow(img, extent=[9, 11, 9, 11])
+    def add_image_to_center(self, image_path):
+        # Resmi harita ortasına eklemek için extent parametresini hesaplayın
+        image = plt.imread(image_path)
+        image_height, image_width = [1, 1]
+        extent = [(self.map_width - image_width) / 2, (self.map_width + image_width) / 2,
+                  (self.map_height - image_height) / 2, (self.map_height + image_height) / 2]
+        self.ax.imshow(image, extent=extent)
 
-plt.show()
+    def update_plot(self):
+        x = random.uniform(0, self.map_height)
+        y = random.uniform(0, self.map_width)
+        self.ax.scatter(x, y, color='red', s=10)
+        self.canvas.draw()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = RealTimePlot()
+    sys.exit(app.exec_())
